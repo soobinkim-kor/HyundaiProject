667bb7bdcd242ebcbadf5ce00511a2abecd81180
li935 <81672839+li935@users.noreply.github.com>
2021-04-01 09:36:08 +0900

    Add files via upload

--- FinalProject/src/com/kosa/LocationDAO.java
+++ FinalProject/src/com/kosa/LocationDAO.java
@@ -8,39 +8,46 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import oracle.jdbc.OracleTypes;
+
 public class LocationDAO {
 	public ArrayList<LocationVO> list() {
 		ArrayList<LocationVO> list = new ArrayList<LocationVO>();
-		
-		String runSP = "{ call sp_select_location(?, ?) }";
-		
+
+		String runSP = "{ call sp_select_location(?, ?, ?) }";
+
 		try {
 			Connection conn = DBConnection.getConnection();
 			CallableStatement callableStatement = conn.prepareCall(runSP);
 
 			callableStatement.registerOutParameter(1, java.sql.Types.NUMERIC);
 			callableStatement.registerOutParameter(2, java.sql.Types.VARCHAR);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
 
 			try {
-				callableStatement.executeQuery();
-					
-				int idx = callableStatement.getInt(1);
-				String city = callableStatement.getString(2);
-				
-				LocationVO data = new LocationVO();
+//				callableStatement.executeQuery();
+				callableStatement.execute();
+				ResultSet resultSet = (ResultSet) callableStatement.getObject(3);
 
-				data.setIdx(idx);
-				data.setCity(city);
+				while (resultSet.next()) {
+					int idx = resultSet.getInt(1);
+					String city = resultSet.getString(2);
 
-				list.add(data);
+					LocationVO data = new LocationVO();
 
-				System.out.println();
-				System.out.println("idx: " + idx);
-				System.out.println("name: " + city);
-				System.out.println();
-				System.out.println("성공");
+					data.setIdx(idx);
+					data.setCity(city);
 
-			// 넘겨줄 객체가 많을 경우 데이터를 VO에 담아서 출력하거나 전달.
+					list.add(data);
+
+					System.out.println();
+					System.out.println("idx: " + idx);
+					System.out.println("name: " + city);
+					System.out.println();
+					System.out.println("성공");
+				}
+
+				// 넘겨줄 객체가 많을 경우 데이터를 VO에 담아서 출력하거나 전달.
 
 			} catch (SQLException e) {
 				System.out.println("프로시저에서 에러 발생!");
@@ -52,7 +59,7 @@ public class LocationDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return list;
 	}
 }
---
+++ FinalProject/src/com/kosa/UsersDAO.java
@@ -0,0 +1,5 @@
+package com.kosa;
+
+public class UsersDAO {
+
+}
