fadcb9989036acf212029d4c06d9ce68604e6127
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-04-01 16:35:21 +0900

    Add files via upload

--- Monopoly/src/application/Main.java
+++ Monopoly/src/application/Main.java
@@ -1,22 +1,25 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		{
-//			Parent root= FXMLLoader.load(getClass().getResource("Root.fxml"));
-			Parent GameScreen= FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
-			Scene scene = new Scene(GameScreen);
-//			Scene scene = new Scene(root);
+			
+			AnchorPane root= FXMLLoader.load(getClass().getResource("root.fxml"));
+			Scene scene = new Scene(root);
+//			Parent GameScreen= FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
+//			Scene scene = new Scene(GameScreen);
+
 //			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Monopoly Game");
 			primaryStage.setResizable(false); 
 			primaryStage.setScene(scene);
